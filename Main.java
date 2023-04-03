9e6779534f0ea4a4f68e25e567eb15a6aa372d2c
Virgile Devolder <virgile.devolder@student.unamur.be>
2023-04-03 14:58:52 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -11,43 +11,45 @@ public class Main {
     }
 
     public static void main(String[] args) throws IOException {
-        read_file(args[0]);
+        ArrayList<ArrayList<String>> flowers = read_file(args[0]);
+        for (ArrayList<String> liste_fleurs : flowers) {
+            String too_much = Analyse(liste_fleurs);
+            print(too_much);
+        }
     }
 
-    public static void read_file(String path) throws IOException {
+    public static ArrayList<ArrayList<String>> read_file(String path) throws IOException {
        
         File file = new File(path);
         Scanner obj = new Scanner(file);
 
         ArrayList<ArrayList<String>> globList = new ArrayList<ArrayList<String>>();
         
-        
+        int i = 0;
         while (obj.hasNextLine()) {
             String line = obj.nextLine();
             
             if (i == 0 || i % 2 == 1) {                
                 i++;
                 int num = Integer.parseInt(line); 
-                print(num);
                 continue;}
             else {
                 String[] tab_values = line.split(" ");
                 ArrayList<String> liste_fleurs = new ArrayList<String>();
                 for (String value : tab_values) {
-                globist.add(liste_fleurs);   
-                
-                liste_fleurs.add(value);
-
+                    liste_fleurs.add(value);  
+                }
+                globList.add(liste_fleurs);  
+            }
             i++;
+        }
+        obj.close();
+        return globList;
+    }
 
-        return globList;     
-    
-
-    
-    
-    public static ing lyse(ArrayList<String> liste_fleurs) {
-        HashMap<Strinnteger> counts = new HashMap<String, Integer>();
-        for (StrinStig plant : liste_fleurs) {
+    public static String Analyse(ArrayList<String> liste_fleurs) {
+        HashMap<String, Integer> counts = new HashMap<String, Integer>();
+        for (String plant : liste_fleurs) {
             counts.put(plant, counts.getOrDefault(plant, 0) + 1);
         }
 
@@ -59,21 +61,5 @@ public class Main {
             }
         }  
         return too_much;
-    }
-        obj.close();
-        }
-    }
-    
-    public static String analyse(ArrayList<String> liste_fleurs) {
-        String too_much = null;
-        for (String plant : counts.keySet()) {
-            if (counts.get(plant) > liste_fleurs.size() / 2) {
-                too_much = plant;
-                break;
-            }
-        }  
-        return too_much;
-    }
-
     }
 }
